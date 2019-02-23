fae765fcba94d540ba94a7c912eda09997e84905
RyanHopkins7 <griffinbrin@gmail.com>
2019-02-23 01:03:14 -0500

    Finalized main

--- src/maze_solver/LA2Main.java
+++ src/maze_solver/LA2Main.java
@@ -9,19 +9,24 @@ public class LA2Main {
 		// gets row and col data from user, creates maze, sets fields in MazeSolver
 		MazeInput.initializeMazeSolver(solver);
 
-		System.out.println("******Maze #1******");
-		System.out.println("Start drawing the maze...");
-		System.out.println("The maze is as below:");
-		solver.printMaze();
-		
-		if (solver.isSolvable()) {
-			solver.solveMazeRecursively(solver.getNumRows(), solver.getNumColumns());
-			solver.printResult();
-		} else {
-			System.out.println("Sorry, no solution can be found for this maze!");
+		for (int i = 1; i <= 3; i++) {
+
+			System.out.println("******Maze #" + i + "******");
+			System.out.println("Start drawing the maze...");
+			System.out.println("The maze is as below:");
+			solver.printMaze();
+
+			if (solver.isSolvable()) {
+				solver.solveMazeRecursively(solver.getNumRows(), solver.getNumColumns());
+				solver.printResult();
+			} else {
+				System.out.println("Sorry, no solution can be found for this maze!");
+			}
+
+			System.out.println();
+
 		}
 
 	}
 
 }
-
