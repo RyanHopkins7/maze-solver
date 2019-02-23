bac218e9d1e0b95b02cfd6cfc020239457db13af
RyanHopkins7 <griffinbrin@gmail.com>
2019-02-23 00:25:22 -0500

    Added drawMaze and printMaze methods to MazeSolver class

--- src/maze_solver/LA2Main.java
+++ src/maze_solver/LA2Main.java
@@ -9,6 +9,9 @@ public class LA2Main {
 		// gets row and col data from user, creates maze, sets fields in MazeSolver
 		MazeInput.initializeMazeSolver(solver);
 
+		System.out.println("******Maze #1******");
+		System.out.println("Start drawing the maze...");
+		System.out.println("The maze is as below:");
 		solver.printMaze();
 		
 		if (solver.isSolvable()) {
--- src/maze_solver/MazeSolver.java
+++ src/maze_solver/MazeSolver.java
@@ -1,44 +1,80 @@
 package maze_solver;
 
 public class MazeSolver {
-	
+
 	private String[][] maze;
 	private int[][] pathScoring;
 	private boolean[][] visited;
 	private int numRows;
 	private int numColumns;
-	
+
 	public void drawMaze() {
+
+		int totalCells = getNumRows() * getNumColumns();
+		int totalObstacles = 0;
+		maze = new String[getNumRows()][getNumColumns()];
+		pathScoring = new int[getNumRows()][getNumColumns()];
+		visited = new boolean[getNumRows()][getNumColumns()];
+
+		for (int i = 0; i < getNumRows(); i++) {
+			for (int j = 0; j < getNumColumns(); j++) {
+
+				if (totalObstacles >= totalCells / 3) {
+					maze[i][j] = String.valueOf((int) (Math.random() * 99 + 1));
+				} else {
+					if (Math.random() < .33) {
+						maze[i][j] = "#";
+						totalObstacles++;
+					} else {
+						maze[i][j] = String.valueOf((int) (Math.random() * 99 + 1));
+					}
+				}
+
+			}
+		}
 		
+		maze[0][0] = "S";
+		maze[getNumRows() - 1][getNumColumns() - 1] = "D";
+
 	}
-	
+
 	public void printMaze() {
-		
+		for (int i = 0; i < getNumRows(); i++) {
+			for (int j = 0; j < getNumColumns(); j++) {
+				System.out.print(maze[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
-	
+
 	public boolean isSolvable() {
 		return solveMazeRecursively(this.getNumRows(), this.getNumColumns());
 	}
-	
+
 	public boolean solveMazeRecursively(int row, int col) {
 		return true;
 	}
-	
+
 	public void printResult() {
-		
+		System.out.println("Congratulations! I found a solution for this maze as below:");
+		printMaze();
+		System.out.println("The amount of coins collected:");
 	}
-	
+
 	// Getters and setters
-	
+
 	public int getNumRows() {
 		return numRows;
 	}
+
 	public void setNumRows(int numRows) {
 		this.numRows = numRows;
 	}
+
 	public int getNumColumns() {
 		return numColumns;
 	}
+
 	public void setNumColumns(int numColumns) {
 		this.numColumns = numColumns;
 	}
