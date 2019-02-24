61055c25ab00a07a12fac33da56fcd3b9fbf114c
RyanHopkins7 <griffinbrin@gmail.com>
2019-02-24 10:47:33 -0500

    Added comments

--- src/maze_solver/LA2Main.java
+++ src/maze_solver/LA2Main.java
@@ -6,15 +6,15 @@ public class LA2Main {
 
 		MazeSolver solver = new MazeSolver();
 
-		// gets row and col data from user, creates maze, sets fields in MazeSolver
+		// Gets row and col data from user, creates maze, sets fields in MazeSolver
 		MazeInput.initializeMazeSolver(solver);
 
 		for (int i = 1; i <= 3; i++) {
 
-			solver.drawMaze();
-
 			System.out.println("******Maze #" + i + "******");
 			System.out.println("Start drawing the maze...");
+			solver.drawMaze();
+			
 			System.out.println("The maze is as below:");
 			solver.printMaze();
 
--- src/maze_solver/MazeSolver.java
+++ src/maze_solver/MazeSolver.java
@@ -4,7 +4,7 @@ public class MazeSolver {
 
 	private String[][] maze;
 	private boolean[][] visited;
-	private double[][] fieldCosts;
+	private double[][] fieldCosts; // Array of cost values for A* algorithm
 	private int numRows;
 	private int numColumns;
 	private int numCoins = 0;
@@ -12,10 +12,12 @@ public class MazeSolver {
 	public void drawMaze() {
 
 		int totalCells = getNumRows() * getNumColumns();
-		int totalObstacles = 0;
+		
 		this.maze = new String[getNumRows()][getNumColumns()];
 		this.visited = new boolean[getNumRows()][getNumColumns()];
 		this.fieldCosts = new double[getNumRows()][getNumColumns()];
+		
+		int totalObstacles = 0;
 
 		for (int i = 0; i < getNumRows(); i++) {
 			for (int j = 0; j < getNumColumns(); j++) {
@@ -23,8 +25,10 @@ public class MazeSolver {
 				this.visited[i][j] = false;
 				this.fieldCosts[i][j] = 99999.9;
 
+				// Number of obstacles cannot be greater than a third of total cells
 				if (totalObstacles >= totalCells / 3) {
-					this.maze[i][j] = String.valueOf((int) (Math.random() * 99 + 1));
+					// Random integer between 1 and 100 representing coins
+					this.maze[i][j] = String.valueOf((int) (Math.random() * 99 + 1)); 
 				} else {
 					if (Math.random() < .25) {
 						this.maze[i][j] = "#";
@@ -37,6 +41,7 @@ public class MazeSolver {
 			}
 		}
 
+		// Set start and destination points
 		this.maze[0][0] = "S";
 		this.maze[getNumRows() - 1][getNumColumns() - 1] = "D";
 		this.visited[0][0] = true;
@@ -63,34 +68,48 @@ public class MazeSolver {
 		calculateRowCost(row - 1, col);
 		calculateRowCost(row, col - 1);
 		
+		// Find lowest cost in fieldCosts that has not already been visited and is not an obstacle
 		double lowestCost = this.fieldCosts[0][0];
 
 		for (int i = 0; i < getNumRows(); i++) {
 			for (int j = 0; j < getNumColumns(); j++) {
-				if (this.fieldCosts[i][j] < lowestCost && !this.maze[i][j].equals("#") && !this.visited[i][j]) {
+				if (this.fieldCosts[i][j] < lowestCost 
+						&& !this.maze[i][j].equals("#") 
+						&& !this.visited[i][j]) {
+					
 					lowestCost = this.fieldCosts[i][j];
+					
 				}
 			}
 		}
 		
+		// If there is no new lowestCost that is not a barrier or has already been visited, end
 		if (lowestCost == this.fieldCosts[0][0]) {
 			return false;
 		}
 		
+		// Move to cell containing lowestCost
 		for (int i = 0; i < getNumRows(); i++) {
 			for (int j = 0; j < getNumColumns(); j++) {
-				if (lowestCost == this.fieldCosts[i][j] && !this.maze[i][j].equals("#") && !this.visited[i][j]) {
+				if (lowestCost == this.fieldCosts[i][j] 
+						&& !this.maze[i][j].equals("#") 
+						&& !this.visited[i][j]) {
+					
+					// If moved to cell containing D in maze, end
 					if (i == getNumRows() - 1 && j == getNumColumns() - 1) {
 						return true;
 					}
+					
 					this.visited[i][j] = true;
 					this.numCoins += Integer.parseInt(maze[i][j]);
 					this.maze[i][j] = "+";
 					solveMazeRecursively(i, j);
+					
 				}
 			}
 		}
 		
+		// Return true if a path to D in maze has been found, else return false
 		if (this.visited[getNumRows() - 2][getNumColumns() - 1] 
 				|| this.visited[getNumRows() - 1][getNumColumns() - 2]) {
 			return true;
@@ -101,6 +120,7 @@ public class MazeSolver {
 	}
 	
 	// Helper method for solveMazeRecursively
+	// Sets cost in fieldCosts for cell in [row, col] if row & col in range and cell is not an obstacle or visited
 	private void calculateRowCost(int row, int col) {
 		if (row < getNumRows() 
 				&& col < getNumColumns()
@@ -109,7 +129,9 @@ public class MazeSolver {
 				&& !this.maze[row][col].equals("#") 
 				&& !this.visited[row][col]) {
 			
+			// Distance from S in maze
 			double distanceFromStart = Math.sqrt(Math.pow(row, 2) + Math.pow(col, 2));
+			// Distance from D in maze
 			double distanceToDestination = Math.sqrt(Math.pow(getNumRows() - row, 2) + Math.pow(getNumColumns() - col, 2));
 			double cost = distanceFromStart + distanceToDestination;
 			
